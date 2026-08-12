eea720a7b3ad781d984a58c19bcece807ea16f8f
Yuyutsu Lamsal <lamsal.yuyutsu38@gmail.com>
2026-08-12 22:38:04 +0545

    default to Client-model

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 import chromadb
-# chroma_client = chromadb.Client()
-chroma_client = chromadb.PersistentClient(path="./chroma_db")  # Specify the path to the persistent database
+chroma_client = chromadb.Client()
+# chroma_client = chromadb.PersistentClient(path="./chroma_db")  # Specify the path to the persistent database
 
-from chromadb.utils import embedding_functions
-sentence_embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
-collection= chroma_client.get_or_create_collection(name="my_collection", embedding_function=sentence_embedding_function)
+# from chromadb.utils import embedding_functions
+# sentence_embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+# collection= chroma_client.get_or_create_collection(name="my_collection", embedding_function=sentence_embedding_function)
 
-# collection_name = 'my_collection'
-# collection = chroma_client.get_or_create_collection(name=collection_name)
+collection_name = 'my_collection'
+collection = chroma_client.get_or_create_collection(name=collection_name)
 
 documents = [
     {"id": "doc1","text": "Hello World"},
-    {"id": "doc2","text": "This is the second document."},
+    {"id": "doc2","text": "This is the hello world document."},
     {"id": "doc3","text": "This is the third document."},
 ]
 
